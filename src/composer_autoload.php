1b51733a2b24be0cd1458fe66a505e76b85e450a
Yoshihiro Saito <saito_yoshihiro@probsc.jp>
2017-01-02 17:32:15 +0900

    Composerでインストールされたときのautoloadを処理

---
+++ src/composer_autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+return function () {
+    $files = array(
+      __DIR__ . '/../../../autoload.php',
+      __DIR__ . '/../vendor/autoload.php',
+    );
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            require_once $file;
+            return true;
+        }
+    }
+    return false;
+};
